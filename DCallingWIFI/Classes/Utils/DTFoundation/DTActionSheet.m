f4ffba29c2192b42dfdd83a4ba84c671615b48be
dalason_prashant <dalason_prashant@02d93018-892b-4fe0-b9ab-f46a02c29a4c>
2015-02-19 13:33:57 +0000

    changed classes

---
+++ DCallingWIFI/Classes/Utils/DTFoundation/DTActionSheet.m
@@ -0,0 +1,203 @@
+//
+//  DTActionSheet.m
+//  DTFoundation
+//
+//  Created by Prashant kumar on 08.06.12.
+//  Copyright (C) 2014 DALASON GmbH.
+//  This file is part of a DALASON Project. (http://www.dalason.de)
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+
+#import "DTActionSheet.h"
+
+@interface DTActionSheet () <UIActionSheetDelegate>
+
+@end
+
+@implementation DTActionSheet
+{
+	id <UIActionSheetDelegate> _externalDelegate;
+	
+	NSMutableDictionary *_actionsPerIndex;
+	
+	// lookup bitmask what delegate methods are implemented
+	struct 
+	{
+		unsigned int delegateSupportsActionSheetCancel:1;
+		unsigned int delegateSupportsWillPresentActionSheet:1;
+		unsigned int delegateSupportsDidPresentActionSheet:1;
+		unsigned int delegateSupportsWillDismissWithButtonIndex:1;
+		unsigned int delegateSupportsDidDismissWithButtonIndex:1;
+	} _delegateFlags;
+}
+
+- (id)init 
+{
+	self = [super init];
+	if (self)
+	{
+		_actionsPerIndex = [[NSMutableDictionary alloc] init];
+		self.delegate = self;
+	}
+	
+	return self;
+}
+
+// designated initializer
+- (id)initWithTitle:(NSString *)title
+{
+	self = [self init];
+	if (self) 
+	{
+		self.title = title;
+	}
+	
+	return self;
+}
+
+- (NSInteger)addButtonWithTitle:(NSString *)title block:(DTActionSheetBlock)block
+{
+	NSInteger retIndex = [self addButtonWithTitle:title];
+	
+	if (block)
+	{
+		NSNumber *key = [NSNumber numberWithInt:retIndex];
+		[_actionsPerIndex setObject:[[block copy] autorelease] forKey:key];
+	}
+	
+	return retIndex;
+}
+
+- (NSInteger)addDestructiveButtonWithTitle:(NSString *)title block:(DTActionSheetBlock)block
+{
+	NSInteger retIndex = [self addButtonWithTitle:title block:block];
+	[self setDestructiveButtonIndex:retIndex];
+	
+	return retIndex;
+}
+
+- (NSInteger)addCancelButtonWithTitle:(NSString *)title block:(DTActionSheetBlock)block
+{
+	NSInteger retIndex = [self addButtonWithTitle:title block:block];
+	[self setCancelButtonIndex:retIndex];
+	
+	return retIndex;
+}
+
+#pragma UIActionSheetDelegate (forwarded)
+
+- (void)actionSheetCancel:(UIActionSheet *)actionSheet
+{
+	if (_delegateFlags.delegateSupportsActionSheetCancel)
+	{
+		[_externalDelegate actionSheetCancel:actionSheet];
+	}
+}
+
+- (void)willPresentActionSheet:(UIActionSheet *)actionSheet
+{
+	if (_delegateFlags.delegateSupportsWillPresentActionSheet)
+	{
+		[_externalDelegate willPresentActionSheet:actionSheet];	
+	}
+}
+
+- (void)didPresentActionSheet:(UIActionSheet *)actionSheet
+{
+	if (_delegateFlags.delegateSupportsDidPresentActionSheet)
+	{
+		[_externalDelegate didPresentActionSheet:actionSheet];
+	}
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet willDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+	if (_delegateFlags.delegateSupportsWillDismissWithButtonIndex)
+	{
+		[_externalDelegate actionSheet:actionSheet willDismissWithButtonIndex:buttonIndex];
+	}
+}
+
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+	NSNumber *key = [NSNumber numberWithInt:buttonIndex];
+	
+	DTActionSheetBlock block = [_actionsPerIndex objectForKey:key];
+	
+	if (block)
+	{
+		block();
+	}
+
+	if (_delegateFlags.delegateSupportsDidDismissWithButtonIndex)
+	{
+		[_externalDelegate actionSheet:actionSheet didDismissWithButtonIndex:buttonIndex];
+	}
+}
+
+
+#pragma mark Properties
+
+- (id <UIActionSheetDelegate>)delegate
+{
+	return _externalDelegate;
+}
+
+- (void)setDelegate:(id <UIActionSheetDelegate>)delegate
+{
+	if (delegate == self)
+	{
+		[super setDelegate:self];
+	}
+	else if (delegate == nil)
+	{
+		[super setDelegate:nil];
+		_externalDelegate = nil;
+	}
+	else 
+	{
+		_externalDelegate = delegate;
+	}
+	
+	// wipe
+	memset(&_delegateFlags, 0, sizeof(_delegateFlags));
+	
+	// set flags according to available methods in delegate
+	if ([_externalDelegate respondsToSelector:@selector(actionSheetCancel:)])
+	{
+		_delegateFlags.delegateSupportsActionSheetCancel = YES;
+	}
+
+	if ([_externalDelegate respondsToSelector:@selector(willPresentActionSheet:)])
+	{
+		_delegateFlags.delegateSupportsWillPresentActionSheet = YES;
+	}
+
+	if ([_externalDelegate respondsToSelector:@selector(didPresentActionSheet:)])
+	{
+		_delegateFlags.delegateSupportsDidPresentActionSheet = YES;
+	}
+
+	if ([_externalDelegate respondsToSelector:@selector(actionSheet:willDismissWithButtonIndex:)])
+	{
+		_delegateFlags.delegateSupportsWillDismissWithButtonIndex = YES;
+	}
+
+	if ([_externalDelegate respondsToSelector:@selector(actionSheet:didDismissWithButtonIndex:)])
+	{
+		_delegateFlags.delegateSupportsDidDismissWithButtonIndex = YES;
+	}
+}
+
+@end
---
+++ DCallingWIFI/Classes/Utils/InAppSettingsKit/Views/IASKPSSliderSpecifierViewCell.m
@@ -0,0 +1,103 @@
+//
+//  IASKPSSliderSpecifierViewCell.m
+//  Created by Prashant on 23/02/12.
+//  Copyright (C) 2014 DALASON GmbH.
+//  This file is part of a DALASON Project. (http://www.dalason.de)
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+
+
+#import "IASKPSSliderSpecifierViewCell.h"
+#import "IASKSlider.h"
+#import "IASKSettingsReader.h"
+
+@implementation IASKPSSliderSpecifierViewCell
+
+@synthesize slider=_slider, 
+            minImage=_minImage, 
+            maxImage=_maxImage;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Setting only frame data that will not be overwritten by layoutSubviews
+        // Slider
+        _slider = [[[IASKSlider alloc] initWithFrame:CGRectMake(0, 0, 0, 23)] autorelease];
+        _slider.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin |
+        UIViewAutoresizingFlexibleWidth;
+        _slider.continuous = NO;
+        [self.contentView addSubview:_slider];
+
+        // MinImage
+        _minImage = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 21, 21)] autorelease];
+        _minImage.autoresizingMask = UIViewAutoresizingFlexibleRightMargin |
+        UIViewAutoresizingFlexibleBottomMargin;
+        [self.contentView addSubview:_minImage];
+
+        // MaxImage
+        _maxImage = [[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 21, 21)] autorelease];
+        _maxImage.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin |
+        UIViewAutoresizingFlexibleBottomMargin;
+        [self.contentView addSubview:_maxImage];
+
+        self.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+	CGRect  sliderBounds    = _slider.bounds;
+    CGPoint sliderCenter    = _slider.center;
+    const double superViewWidth = _slider.superview.frame.size.width;
+    
+    sliderCenter.x = superViewWidth / 2;
+    sliderCenter.y = self.contentView.center.y;
+    sliderBounds.size.width = superViewWidth - kIASKSliderNoImagesPadding * 2;
+	_minImage.hidden = YES;
+	_maxImage.hidden = YES;
+
+	// Check if there are min and max images. If so, change the layout accordingly.
+	if (_minImage.image) {
+		// Min image
+		_minImage.hidden = NO;
+		sliderCenter.x    += (kIASKSliderImagesPadding - kIASKSliderNoImagesPadding) / 2;
+		sliderBounds.size.width  -= (kIASKSliderImagesPadding - kIASKSliderNoImagesPadding);
+        _minImage.center = CGPointMake(_minImage.frame.size.width / 2 + kIASKPaddingLeft,
+                                       self.contentView.center.y);
+    }
+	if (_maxImage.image) {
+		// Max image
+		_maxImage.hidden = NO;
+		sliderCenter.x    -= (kIASKSliderImagesPadding - kIASKSliderNoImagesPadding) / 2;
+		sliderBounds.size.width  -= (kIASKSliderImagesPadding - kIASKSliderNoImagesPadding);
+        _maxImage.center = CGPointMake(self.contentView.bounds.size.width - _maxImage.frame.size.width / 2 - kIASKPaddingRight,
+                                       self.contentView.center.y);
+	}
+	
+	_slider.bounds = sliderBounds;
+    _slider.center = sliderCenter;
+}	
+
+- (void)dealloc {
+	_minImage.image = nil;
+	_maxImage.image = nil;
+    [super dealloc];
+}
+
+- (void)prepareForReuse {
+	_minImage.image = nil;
+	_maxImage.image = nil;
+}
+@end
